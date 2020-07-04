06dd7b9c5c5d052fd5ece03ca06753f99eef28ba
Peter J De Sousa <petedes@live.com>
2020-07-04 18:13:04 +0100

    Tutorial

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/maxence-charriere/go-app/v7/pkg/app"
+)
+
+func main() {
+
+	h := &app.Handler{
+		Title:  "Hello Demo",
+		Author: "Maxence Charriere",
+	}
+
+	if err := http.ListenAndServe(":7777", h); err != nil {
+		panic(err)
+	}
+}
